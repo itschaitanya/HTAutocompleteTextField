8bf40ec74a6c6e5b9fbff2d2c2f8be3a086b4723
Chaitanya Ramavajjala <chaitanyar@zillow.com>
2015-11-01 18:58:08 -0800

    Making it a framework.

---
+++ HTAutocompleteTextFieldKit/HTAutocompleteTextFieldKit.h
@@ -0,0 +1,21 @@
+//
+//  HTAutocompleteTextFieldKit.h
+//  HTAutocompleteTextFieldKit
+//
+//  Created by Chaitanya Ramavajjala on 10/23/15.
+//  Copyright © 2015 Hotel Tonight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTAutocompleteTextFieldKit.
+FOUNDATION_EXPORT double HTAutocompleteTextFieldKitVersionNumber;
+
+//! Project version string for HTAutocompleteTextFieldKit.
+FOUNDATION_EXPORT const unsigned char HTAutocompleteTextFieldKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTAutocompleteTextFieldKit/PublicHeader.h>
+
+
+#import <HTAutocompleteTextFieldKit/HTAutocompleteTextField.h>
+#import <HTAutocompleteTextFieldKit/HTEmailAutocompleteTextField.h>
